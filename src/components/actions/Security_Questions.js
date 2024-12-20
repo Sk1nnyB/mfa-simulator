197cfaa88ce9f7c8c1904b069a270240fe879cff
Ben Barrow <114662236+Sk1nnyB@users.noreply.github.com>
2024-12-20 16:07:20 +0000

    Duplicate Efforts in Password for Security Questions
    - Also added checks to ensure long enough answer and question was selected

--- src/components/actions/Security_Questions.js
+++ src/components/actions/Security_Questions.js
@@ -7,26 +7,40 @@ function Security_Questions() {
   const [selectedQuestion, setSelectedQuestion] = useState('');
   const [savedAnswer, setSavedAnswer] = useState('');
   const [inputAnswer, setInputAnswer] = useState('');
+  const [validAnswer, setValidAnswer] = useState(false);
 
   const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const context = queryParams.get('context');
 
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSavedAnswer(value);
+    value.length < 3 ? setValidAnswer(false) : setValidAnswer(true);
+  };
+
   const handleInputClick = () => {
+    if (!selectedQuestion) {
+      alert('No question selected.');
+      return;
+    }
+
     if (!savedAnswer || !inputAnswer) {
       alert('No answer given for the security question.');
       return;
     }
 
+    if (!validAnswer) {
+      alert('Given answer is too short!');
+      return;
+    }
+
+
     if (savedAnswer === inputAnswer) {
       let pos = parseInt(context[context.length - 1], 16);
       const next = (parseInt(context, 16) + 1).toString(16).toUpperCase().padStart(4, '0');
-      if (pos === 0) {
-        navigate(`/play?context=${next}`);
-      } else {
-        navigate(`/play?context=${next}`, { replace: true });
-      }
+      pos === 0 ? navigate(`/play?context=${next}`) : navigate(`/play?context=${next}`, { replace: true });
     } else {
       alert(`Entered Answer: ${inputAnswer} is not correct! Try again.`);
     }
@@ -34,7 +48,10 @@ function Security_Questions() {
 
   return (
     <div className="questions-container">
-      <div className="questions-select-container">
+      <div className="questions-select-container" style={{
+        backgroundColor: validAnswer ? (selectedQuestion ? "#dbdfe2" : "#b1e9fa") : "#b1e9fa",
+      }}>
+        <h2>Step 1: Select Your Question</h2>
         <label htmlFor="select-question">Select question:</label>
         <select
           id="select-question"
@@ -54,11 +71,14 @@ function Security_Questions() {
           id="saved-answer"
           placeholder="Enter Here!"
           value={savedAnswer}
-          onChange={(e) => setSavedAnswer(e.target.value)}
+          onChange={handleInputChange}
         />
       </div>
 
-      <div className="questions-input-container">
+      <div className="questions-input-container" style={{
+        backgroundColor: validAnswer ? (selectedQuestion ? "#b1e9fa" : "#dbdfe2") : "#dbdfe2",
+      }}>
+        <h2>Step 2: Answer Chosen Question</h2>
         <label htmlFor="input-answer">
           Security Question: <span>{selectedQuestion || 'XXX'}</span>
         </label>
