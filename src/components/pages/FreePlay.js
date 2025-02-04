59b76f5def5e669a47c51373a378d11621c9ac7c
Sk1nnyB <114662236+Sk1nnyB@users.noreply.github.com>
2025-02-04 02:08:00 +0000

    Improve freeplay
    Added tool tips to make it easier to use and more complex

--- src/components/pages/FreePlay.js
+++ src/components/pages/FreePlay.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./FreePlay.css";
 import { optionsMFA } from '../../data/options_mfa';
 import { useNavigate } from 'react-router-dom';
+import { Tooltip } from 'react-tooltip'
 
 function FreePlay() {
   const defaultLinkStub = "https://sk1nnyb.github.io/mfa-simulator/#/";
@@ -9,6 +10,7 @@ function FreePlay() {
   const [options, setOptions] = useState(Array(optionsMFA.length).fill(false));
   const [setupLink, setSetupLink] = useState(defaultLink);
   const [playcode, setPlaycode] = useState(0);
+  const [authenticationLevel, setAuthenticationLevel] = useState(0);
   const navigate = useNavigate();
 
   const toggleOption = (index) => {
@@ -22,10 +24,24 @@ function FreePlay() {
 
         if (newPlaycode === 0) {
           setSetupLink(defaultLink);
+          setAuthenticationLevel(0);
         } else {
           let hexPlaycode = (newPlaycode * 16).toString(16);
           hexPlaycode = hexPlaycode.padStart(4, '0');
           setSetupLink(defaultLinkStub+"play?context="+hexPlaycode+"&startPage=1");
+
+          // [Password(0), Sec Ques(1), Auth App(2), Text(3), Email(4), Fingerprint(5), Smart Card(6), Voice(7)]
+          let categories = 0;
+          if (newOptions[0]) {
+            categories += 1;
+          }
+          if (newOptions[2] || newOptions[3] || newOptions[4] || newOptions[6]) {
+            categories += 1;
+          }
+          if (newOptions[5] || newOptions[7]) {
+            categories += 1;
+          }
+          setAuthenticationLevel(categories);
         }
 
         return newPlaycode;
@@ -50,6 +66,16 @@ function FreePlay() {
     <div className="freeplay">
       <div className="left-container">
         <div className="box-border options-container">
+          <a
+              data-tooltip-id="ins-tooltip"
+              data-tooltip-html="Choose your options<br />
+                                If you don't like them, reset<br />
+                                Then click start<br />
+                                We'll do it no sweat!"
+              data-tooltip-place="top"
+              className="tooltip-circle tooltip-circle-ins"
+            > ? </a>
+          <Tooltip id="ins-tooltip"/>
           {optionsMFA.map((option, index) => (
             <div key={index} className="option">
               <label className="switch">
@@ -64,18 +90,19 @@ function FreePlay() {
             </div>
           ))}
         </div>
-        <div className="box-border instructions-container">
-          <h2>What to do?</h2>
-          <p>Choose your options<br />
-          If you don't like them, reset<br />
-          Then click start<br />
-          We'll do it no sweat!</p>
-        </div>
       </div>
       <div className="right-container">
         <div className="assurance-container">
+          <a
+            data-tooltip-id="aa-tooltip"
+            data-tooltip-html="Authentication Assurance Levels are an indicator of how strong the set up is.
+                              <br /> NOTE: Security Questions do not count as they are not strong enough."
+            data-tooltip-place="top"
+            className="tooltip-circle tooltip-circle-aa"
+          > ? </a>
+          <Tooltip id="aa-tooltip"/>
           <h2>Authentication Assurance Level</h2>
-          <h3>1</h3>
+          <h3 className={`auth-level-${authenticationLevel}-color`}>{authenticationLevel}</h3>
         </div>
         <div className="buttons-container">
           <button onClick={handleStartClick} disabled={playcode === 0} className="freeplay-start-button primary-button">
